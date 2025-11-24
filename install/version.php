ab82fcda1be9414af6afdaedebe1e6b35769e2ac
r3ct4lan <smolkov.dev@gmail.com>
2025-11-24 19:32:31 +0100

    docs: добавлен файл README.md с бэклогом задач
    Добавлен новый файл README.md, содержащий список задач

--- install/version.php
+++ install/version.php
@@ -7,7 +7,7 @@ return [
     'MODULE_ID' => 'orm.annotate',
     'MODULE_NAME' => Loc::getMessage('MODULE_NAME'),
     'MODULE_DESCRIPTION' => Loc::getMessage('MODULE_DESCRIPTION'),
-    'MODULE_VERSION' => '0.0.3',
+    'MODULE_VERSION' => '0.0.4',
     'MODULE_VERSION_DATE' => '2025-11-18',
     'PARTNER_NAME' => Loc::getMessage('PARTNER_NAME'),
     'PARTNER_URI' => 'https://github.com/r3ct4lan/orm.annotate',
